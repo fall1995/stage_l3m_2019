0e3c80d71d6203699224ca28592f76eff34d6926
fall199 <alyabderrahmanefall@gmail.com>
2019-06-19 16:24:08 +0200

    DNS ne marche pas

--- versionfinal/versionfinal.ino
+++ versionfinal/versionfinal.ino
@@ -3,10 +3,10 @@
 #include <ESP8266WebServer.h>
 #include "index.h"
 #include <SPI.h>
-#include <Ethernet.h>
 #include <DNSServer.h>
 #include <ESP8266WiFiMulti.h>   // Include the Wi-Fi-Multi library
 #include <ESP8266mDNS.h>        // Include the mDNS library
+
 
 
 /* cablage des broches de la carte de puissance TB6612FNG vers la carte arduino */
@@ -18,13 +18,14 @@
 #define APSSID "iPhone de Aly"
 #define APPSK  "af123456789"
 #endif
-
+MDNSResponder mdns;
 /* Set these to your desired credentials. */
 const char *ssid = APSSID;
 const char *password = APPSK;
 const byte DNS_PORT = 53;
-IPAddress apIP(192, 168, 4, 1);
-DNSServer dnsServer;
+
+//IPAddress apIP(172, 20, 10, 6);
+//DNSServer dnsServer;
 
 ESP8266WebServer server(80);
 
@@ -44,15 +45,10 @@ void setup()
   pinMode(STBY, OUTPUT);
   // Ouvre la voie série avec l'ordinateur
   Serial.begin(115200);
-  server.on("/", handleRoot);
-  server.on("/startUp", startUp);
-  server.on("/shutDown", shutDown);
-  server.on("/goForward", goForward);
-  server.on("/goBackward", goBackward);
-  server.on("/accelerer", accelerer);
-  server.on("/ralentir", ralentir);
+  
   //server.begin();
   //  Connect to Wi-Fi network with SSID and password
+  //WiFi.mode(WIFI_STA);
   Serial.print("Connecting to ");
   Serial.println(ssid);
   WiFi.begin(ssid, password);
@@ -77,15 +73,45 @@ void setup()
   Serial.println("WiFi connected.");
   Serial.println("IP address: ");
   Serial.println(WiFi.localIP());
+  //-------DNS ne marche pas pour l'instant
+  /*
+  WiFi.softAPConfig(apIP, apIP, IPAddress(255, 255, 255, 0));
+  WiFi.softAP("Moteur");
+  dnsServer.setTTL(300);
+  dnsServer.setErrorReplyCode(DNSReplyCode::ServerFailure);
+  // start DNS server for a specific domain name
+
+  dnsServer.start(DNS_PORT, "www.mote.com", WiFi.localIP());
   //--------------
+    */
+  // DNS
+   if(!MDNS.begin("esp8266")){             // Start the mDNS responder for esp8266.local
+    Serial.println("Error setting up MDNS responder!");
+  }
+  Serial.println("mDNS responder started");
+  /*if (mdns.begin("esp8266", WiFi.localIP())) {
+    Serial.println("MDNS started");
+  
+  }*/
+ 
+  server.on("/", handleRoot);
+  server.on("/startUp", startUp);
+  server.on("/shutDown", shutDown);
+  server.on("/goForward", goForward);
+  server.on("/goBackward", goBackward);
+  server.on("/accelerer", accelerer);
+  server.on("/ralentir", ralentir);
   
   server.begin();
+
+  // Add service to MDNS-SD
+  //MDNS.addService("http", "tcp", 80);
 }
 
 
 void loop()
 {
-  dnsServer.processNextRequest();
+ // dnsServer.processNextRequest();
   server.handleClient();
   
 }
